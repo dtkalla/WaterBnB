d0c9c0997da58ed34816c4b7122a902b58b34e1e
Daniel <danieltkalla@gmail.com>
2022-12-05 12:54:43 -0500

    start modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,13 @@ import Navigation from "./components/Navigation";
 function App() {
   return (
     <>
+      <modal id='modal'>
+        <button id="modalButton">
+          <img id="modalButtonImage" src="https://cdn-icons-png.flaticon.com/512/3524/3524335.png"/>
+        </button>
+      </modal>
       <Navigation />
+
         <Switch>
           <Route path="/login">
             <LoginFormPage />
