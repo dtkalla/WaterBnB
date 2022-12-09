d793f885aaf8af4c9f370eb1c615a5a6115db7a4
Daniel <danieltkalla@gmail.com>
2022-12-09 14:27:43 -0500

    R functionality for listings

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -2,15 +2,15 @@ class Api::ListingsController < ApplicationController
     wrap_parameters include: Listing.attribute_names + ['buildingType']
     before_action :require_logged_in, only: [:create, :update, :destroy]
   
-    def create
-        lister_id, lister_name = current_user.id, current_user.first_name
-        @listing = Listing.new(listing_params, lister_id: lister_id, lister_name: lister_name)
-        if @listing.save
-            render :show
-        else
-            render json: { errors: @listing.errors.full_messages}, status: 422
-        end
-    end
+    # def create
+    #     lister_id, lister_name = current_user.id, current_user.first_name
+    #     @listing = Listing.new(listing_params, lister_id: lister_id, lister_name: lister_name)
+    #     if @listing.save
+    #         render :show
+    #     else
+    #         render json: { errors: @listing.errors.full_messages}, status: 422
+    #     end
+    # end
 
     def index
         @listings = Listing.all
@@ -22,19 +22,19 @@ class Api::ListingsController < ApplicationController
         render :show
     end
 
-    def update
+    # def update
 
-    end
+    # end
 
-    def destroy
+    # def destroy
 
-    end
+    # end
   
   
-    private
+    # private
   
-    def listing_params
-        params.require(:listing).permit(:city, :country, :latitude, :longitude, :description, :pictures_url, :price, :bedrooms, :beds, :bathrooms, :building_type, :type_of_water, :kitchen, :wifi, :parking, :pets_allowed, :boat, :self_check_in, :lister_name)
-    end
+    # def listing_params
+    #     params.require(:listing).permit(:city, :country, :latitude, :longitude, :description, :pictures_url, :price, :bedrooms, :beds, :bathrooms, :building_type, :type_of_water, :kitchen, :wifi, :parking, :pets_allowed, :boat, :self_check_in, :lister_name)
+    # end
 end
   
