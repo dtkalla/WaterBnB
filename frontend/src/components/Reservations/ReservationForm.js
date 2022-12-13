063cbbd9d8ce0919247874d8fe5734a5d0adfe6a
Daniel <danieltkalla@gmail.com>
2022-12-13 01:19:12 -0500

    notice to login on res form

--- frontend/src/components/Reservations/ReservationForm.js
+++ frontend/src/components/Reservations/ReservationForm.js
@@ -52,80 +52,90 @@ function ReservationForm() {
   const currentDate = new Date()
 
 
-  return (sessionUser && 
-    <form id='reservation-form' onSubmit={handleSubmit}>
-      {/* <input type="hidden" value={listingId} />
-
-      <input type="hidden" value={reserverId} /> */}
-      <ul>
-        <div id='reservation-price'>
-          <span><b>${listing.price}</b> night</span>
-        </div>
-        <br/>
-        <div id='start-end-dates'>
-          <div id='checkin-div'>
-            <div id='other-small-font'>CHECK-IN</div>
-            <label>
-            <input className="date-input" type="date" value={startDate}
-            onChange={(e) => setStartDate(e.target.value)} required 
-            placeholder={currentDate}
-            min={currentDate}/>
-            </label>
+  if (sessionUser) {
+    return ( 
+      <form id='reservation-form' onSubmit={handleSubmit}>
+        {/* <input type="hidden" value={listingId} />
+  
+        <input type="hidden" value={reserverId} /> */}
+        <ul>
+          <div id='reservation-price'>
+            <span><b>${listing.price}</b> night</span>
           </div>
-          <div className='vertical-line-reservations-2'></div>
-          <div>
-            <div id='other-small-font'>CHECKOUT</div>
-            <label>
-            <input className="date-input" type="date" value={endDate}
-            onChange={(e) => {
-              setEndDate(e.target.value)
-            }} required 
-            min={startDate}/>
-            </label>
+          <br/>
+          <div id='start-end-dates'>
+            <div id='checkin-div'>
+              <div id='other-small-font'>CHECK-IN</div>
+              <label>
+              <input className="date-input" type="date" value={startDate}
+              onChange={(e) => setStartDate(e.target.value)} required 
+              placeholder={currentDate}
+              min={currentDate}/>
+              </label>
+            </div>
+            <div className='vertical-line-reservations-2'></div>
+            <div>
+              <div id='other-small-font'>CHECKOUT</div>
+              <label>
+              <input className="date-input" type="date" value={endDate}
+              onChange={(e) => {
+                setEndDate(e.target.value)
+              }} required 
+              min={startDate}/>
+              </label>
+            </div>
           </div>
-        </div>
-          <div id='num-guests'>
-            <div id='other-small-font'>GUESTS</div>
-            <label>
-              <input type="number"
-              onChange={(e) => setNumGuests(e.target.value)} required
-              min="1" max={listing.beds + 1}  placeholder='1' />
-            </label>
-          </div>
-
-        <br/>
-        <ul className="fees">
-          <span className="underline">
-            ${listing.price + (numGuests ? (numGuests-1)*10 : 0)} X {(startDate && endDate) ? endDate.slice(8) - startDate.slice(8) : 0} nights
-          </span>
-          <span>
-            {(startDate && endDate) ? '$'+(endDate.slice(8) - startDate.slice(8))*(listing.price + (numGuests ? (numGuests-1)*10 : 0)) : '$0'}
-          </span>
-          <span className="underline">{listing.boat ? "Boating fee" : ''}</span>
-          <span>{listing.boat ? '$20' : ''}</span>
-          <span className="underline">Service fee</span>
-          <span>{'$' + Math.floor(((startDate && endDate) ? endDate.slice(8) - startDate.slice(8) : 0) * (listing.price + (numGuests ? (numGuests-1)*10 : 0)) / 7)}</span>
-          <div className='solid-line-reservations-2'></div>
-          <span></span>
-        <span>Total cost</span>
-          <span>
-            {'$' + Math.floor(((startDate && endDate) ? endDate.slice(8) - startDate.slice(8) : 0) * (listing.price + (numGuests ? (numGuests-1)*10 : 0)) * 8 / 7 + (listing.boat ? 20 : 0))}
-          </span>
+            <div id='num-guests'>
+              <div id='other-small-font'>GUESTS</div>
+              <label>
+                <input type="number"
+                onChange={(e) => setNumGuests(e.target.value)} required
+                min="1" max={listing.beds + 1}  placeholder='1' />
+              </label>
+            </div>
+  
+          <br/>
+          <ul className="fees">
+            <span className="underline">
+              ${listing.price + (numGuests ? (numGuests-1)*10 : 0)} X {(startDate && endDate) ? endDate.slice(8) - startDate.slice(8) : 0} nights
+            </span>
+            <span>
+              {(startDate && endDate) ? '$'+(endDate.slice(8) - startDate.slice(8))*(listing.price + (numGuests ? (numGuests-1)*10 : 0)) : '$0'}
+            </span>
+            <span className="underline">{listing.boat ? "Boating fee" : ''}</span>
+            <span>{listing.boat ? '$20' : ''}</span>
+            <span className="underline">Service fee</span>
+            <span>{'$' + Math.floor(((startDate && endDate) ? endDate.slice(8) - startDate.slice(8) : 0) * (listing.price + (numGuests ? (numGuests-1)*10 : 0)) / 7)}</span>
+            <div className='solid-line-reservations-2'></div>
+            <span></span>
+          <span>Total cost</span>
+            <span>
+              {'$' + Math.floor(((startDate && endDate) ? endDate.slice(8) - startDate.slice(8) : 0) * (listing.price + (numGuests ? (numGuests-1)*10 : 0)) * 8 / 7 + (listing.boat ? 20 : 0))}
+            </span>
+          </ul>
+          
+          
+          <br/>
+          <ul className="form-errors">
+            {errors.map(error => <li key={error}>{error}</li>)}
+          </ul>
+          <br/>
+          
+          <button className="reservation-button" type="submit">Reserve</button>
         </ul>
         
         
-        <br/>
-        <ul className="form-errors">
-          {errors.map(error => <li key={error}>{error}</li>)}
-        </ul>
-        <br/>
-        
-        <button className="reservation-button" type="submit">Reserve</button>
-      </ul>
-      
-      
-    </form>
-  );
+      </form>
+    );
+  } else {
+    return (
+      <div id='reservation-form' className="res-login-notice">
+        <h3>Please login to reserve today!</h3>
+      </div>
+    )
+  }
+
+  
 }
 
 export default ReservationForm;
