4b8fbe0b4397b9ecec9daf565bb351b9e0f78479
Daniel <danieltkalla@gmail.com>
2022-12-12 21:58:53 -0500

    map works with my data!

--- frontend/src/components/Listings/ListingIndex.js
+++ frontend/src/components/Listings/ListingIndex.js
@@ -17,43 +17,56 @@ const ListingIndex = () => {
         dispatch(fetchListings())
     }, [])
 
-    const locations = [
-        {
-          name: "Location 1",
-          location: { 
-            lat: 41.3954,
-            lng: 2.162 
-          },
-        },
-        {
-          name: "Location 2",
-          location: { 
-            lat: 41.3917,
-            lng: 2.1649
-          },
-        },
-        {
-          name: "Location 3",
-          location: { 
-            lat: 41.3773,
-            lng: 2.1585
-          },
-        },
-        {
-          name: "Location 4",
-          location: { 
-            lat: 41.3797,
-            lng: 2.1682
-          },
-        },
-        {
-          name: "Location 5",
-          location: { 
-            lat: 41.4055,
-            lng: 2.1915
-          },
-        }
-      ];
+    // const locations = [
+    //     {
+    //       name: "Location 1",
+    //       location: { 
+    //         lat: 41.3954,
+    //         lng: 2.162 
+    //       },
+    //     },
+    //     {
+    //       name: "Location 2",
+    //       location: { 
+    //         lat: 41.3917,
+    //         lng: 2.1649
+    //       },
+    //     },
+    //     {
+    //       name: "Location 3",
+    //       location: { 
+    //         lat: 41.3773,
+    //         lng: 2.1585
+    //       },
+    //     },
+    //     {
+    //       name: "Location 4",
+    //       location: { 
+    //         lat: 41.3797,
+    //         lng: 2.1682
+    //       },
+    //     },
+    //     {
+    //       name: "Location 5",
+    //       location: { 
+    //         lat: 41.4055,
+    //         lng: 2.1915
+    //       },
+    //     }
+    //   ];
+
+    const locations = []
+
+    for (let i = 0; i < listings.length; i++) {
+        locations.push({
+            name: listings[i].id,
+            location: {
+                lat: parseFloat(listings[i].latitude),
+                lng: parseFloat(listings[i].longitude)
+            }
+        })
+        console.log(locations)
+    }
 
     const listingItems = listings.map((listing) => {
         return <ListingIndexItem key={listing.id} listing={listing} />
