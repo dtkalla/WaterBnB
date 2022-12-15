307ff7f8b3a7d72ffba03fbc4b4eac0ae0baab7f
Daniel <danieltkalla@gmail.com>
2022-12-15 10:13:12 -0500

    have separate filter page, doesn't look great

--- frontend/src/components/FilteredListings/FilteredListings.js
+++ frontend/src/components/FilteredListings/FilteredListings.js
@@ -1,16 +1,21 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 // import { Link } from 'react-router-dom';
-import ListingIndexItem from './ListingIndexItem';
+import ListingIndexItem from '../Listings/ListingIndexItem';
 import { fetchListings, getListings } from '../../store/listings';
-import './listings.css'
+import '../Listings/listings.css'
 import MapContainer from '../MapContainer/MapContainer';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 
 
 
-const ListingIndex = () => {
+const FilteredListings = () => {
     const dispatch = useDispatch();
+    const filter = useParams();
+    console.log(filter)
     const listings = useSelector(getListings)
+    .filter((listing) => listing.typeOfWater == filter.filter ? listing : null)
+    console.log(listings)
 
     useEffect(() => {
         dispatch(fetchListings())
@@ -18,7 +23,16 @@ const ListingIndex = () => {
 
     const locations = []
 
+    const listingItems = listings.map((listing) => {
+        if (listing) {
+            return <ListingIndexItem key={listing.id} listing={listing} />
+        } else {
+            return null
+        }
+    })
+
     for (let i = 0; i < listings.length; i++) {
+        if (listings[i]) {
         locations.push({
             name: listings[i].id,
             title: listings[i].listerName+"'s " + listings[i].buildingType,
@@ -30,12 +44,10 @@ const ListingIndex = () => {
                 lat: parseFloat(listings[i].latitude),
                 lng: parseFloat(listings[i].longitude)
             }
-        })
+        })}
     }
 
-    const listingItems = listings.map((listing) => {
-        return <ListingIndexItem key={listing.id} listing={listing} />
-    })
+    
 
     return (
         <div className='listings-index'>
@@ -52,4 +64,4 @@ const ListingIndex = () => {
 }
 
 
-export default ListingIndex
+export default FilteredListings
