a6f1fd628ac278c15611298c9eae4c4dd72a29ec
Daniel <danieltkalla@gmail.com>
2022-12-16 11:21:48 -0500

    fixed icon, hopefully works in Render

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,12 +1,13 @@
 // import { useState } from 'react';
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import "./Header.css";
+import logo from './waterbnb_logo.png'
 
 
 function Header( props ) {
     return (
         <Link id='logo' to='/'>
-            <img id='logo-img' src="http://clipart-library.com/img/1699067.png" />
+            <img id='logo-img' src={logo} />
             <span id='span-logo'>waterbnb</span>
         </Link>
     )
