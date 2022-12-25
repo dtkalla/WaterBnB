a79c26320ee762baeb0d423f762854e34166a7e2
Daniel <danieltkalla@gmail.com>
2022-12-25 00:01:36 -0600

    add profile pic to navbar if !!current_user

--- frontend/src/components/Dropdown/Dropdown.js
+++ frontend/src/components/Dropdown/Dropdown.js
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Navigation from '../Navigation';
 import "./Dropdown.css";
+import { useSelector } from 'react-redux';
 
 
 function Dropdown( props ) {
     const [ display, setDisplay ] = useState( 'none' )
+    const sessionUser = useSelector(state => state.session.user);
     function handleClick() {
         if ( display === 'none' ) {
             setDisplay( 'block' )
@@ -22,7 +24,7 @@ function Dropdown( props ) {
         <div>
             <div className='dropdown-button' onClick={handleClick}>
                     <img id='hamburger' src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png" />
-                    <i className="fa-solid fa-user-circle fa-xl" />
+                    <img id='profile-dropdown-pic' src={sessionUser ? sessionUser.profilePic : 'https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg'} />
                 </div>
             <div className='dropdown-menu' onClick={handleClick2} style={{display:display}}>
                 <div id="modal2-background" />
