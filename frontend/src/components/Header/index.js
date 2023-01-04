3173b49455cf5f6cb934cdbe55b60965b9b330a1
Daniel <danieltkalla@gmail.com>
2023-01-04 12:47:40 -0500

    update LinkedIn url

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -15,7 +15,7 @@ function Header( props ) {
             <img className="icons-header" alt="Github" src="https://cdn-icons-png.flaticon.com/512/25/25231.png"/>
         </a>
         <br/>
-        <a href="https://www.linkedin.com/in/daniel-kalla-496aa7234/" target="_blank">
+        <a href="www.linkedin.com/in/daniel-kalla" target="_blank">
             <img className="icons-header" alt="LinkedIn" src="https://cdn-icons-png.flaticon.com/512/174/174857.png"/>
         </a>
         </div>
